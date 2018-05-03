da93af214746919fcb6495dddcbfa61810414320
wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2018-05-02 22:11:02 -0300

    Clase item

---
+++ JavaApplication2/src/model/Item.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author wagner
+ */
+public class Item {
+    private Livro livro;
+    private int numero;
+    private float valorItem;
+    private int quantidade;
+
+    public Item() {
+    }
+
+    public Item(Livro livro) {
+        this.livro = livro;
+    }
+    public void calcularValorItem(){
+        valorItem = livro.getValorUnitario() * quantidade;
+    }
+    
+    public String toString(){
+        String str = "";
+        str += "\nTitulo do livro:" + livro.getTitulo();
+        str += "\nValor unitario" + livro.getValorUnitario();
+        str += "\nEm estoque"+ livro.getQuantidadeEst();
+        str += "\nQuntidade: "+ quantidade;
+        str += "\nvalor do item" + valorItem;
+        str += "\n ##################### \n";
+        
+        return str;
+
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public float getValorItem() {
+        return valorItem;
+    }
+
+    public void setValorItem(float valorItem) {
+        this.valorItem = valorItem;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+}
